af5f6268ea35a3cc326b17a113dcc2c2739009ae
student <student@DESKTOP-QCKBJ2Q>
2017-11-02 11:03:17 +0100

    RestCountries

---
+++ RestCountries/RestCountries/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace RestCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader oSr = new StreamReader("countries.json");
+            string sJson = "";
+            using (oSr)
+            {
+                sJson = oSr.ReadToEnd();
+            }
+            JObject oJson = JObject.Parse(sJson);
+            var oCountries = oJson["countries"].ToList();
+            List<country> lCountry = new List<country>();
+
+            for (int i=0; i< oCountries.Count; i++)
+            {
+                lCountry.Add(new country
+                { 
+                
+                    sCode = (string)oCountries[i]["alpha3Code"],
+                    sName = (string)oCountries[i]["name"],
+                    sCapital = (string)oCountries[i]["capital"],
+                    nPopulation = (int)oCountries[i]["population"],
+                    fArea = (float)oCountries[i]["area"],
+                });
+            }
+
+            for(int i=0; i<oCountries.Count();i++)
+            {
+                Console.WriteLine(lCountry[i].sCode + " " + lCountry[i].sName);
+            }
+            Console.WriteLine("-----------------------------------------");
+            var OrderByQuery = from c in lCountry.OrderByDescending(o => o.nPopulation) select c;
+            List<country> lSortedCountry = OrderByQuery.ToList();
+            for(int i= 0; i< lSortedCountry.Count; i++)
+            {
+                Console.WriteLine(lSortedCountry[i].nPopulation +" " +lSortedCountry[i].sName + " " + lSortedCountry[i].sCapital);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RestCountries/RestCountries/country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestCountries
+{
+    class country
+    {
+        public string sCode { get; set; }
+        public string sName { get; set; }
+        public string sCapital { get; set; }
+        public int nPopulation { get; set; }
+        public float fArea { get; set; }
+    }
+}
